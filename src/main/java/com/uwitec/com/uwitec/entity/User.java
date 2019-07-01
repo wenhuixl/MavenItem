dc09a89820b8cf53e81b524f64c0e66f0497f055
wenhui <wenhuigl@gmail.com>
2019-07-01 21:52:36 +0800

    添加lombok

---
+++ src/main/java/com/uwitec/com/uwitec/entity/User.java
@@ -0,0 +1,35 @@
+package com.uwitec.com.uwitec.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author wenhui
+ * @description
+ * @Date 2019/7/1
+ */
+@Slf4j
+@Getter
+@Setter
+public class User {
+
+    private String name;
+    private Integer age;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        User user = new User();
+        user.setName("zhangsan");
+        user.setAge(23);
+        log.info(user.toString());
+    }
+
+}
