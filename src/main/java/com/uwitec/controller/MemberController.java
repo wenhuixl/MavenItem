911b86e5e366c07aadc8311a25b00d3801021426
wenhui <wenhuigl@gmail.com>
2019-07-03 22:22:14 +0800

    区分不同环境配置

--- src/main/java/com/uwitec/controller/MemberController.java
+++ src/main/java/com/uwitec/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.uwitec.controller;
 import com.uwitec.service.MemberService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,13 @@ import java.util.Map;
 public class MemberController {
 
     @Autowired
-    MemberService memberService;
+    private MemberService memberService;
+
+    @Value("${name}")
+    private String name;
+
+    @Value("${http_url}")
+    private String httpUrl;
 
     @RequestMapping("/getMember")
     public Map<String, Object> getMember() {
@@ -30,6 +37,16 @@ public class MemberController {
         String result = memberService.addMemberAndEmail();
         log.info("4");
         return "result: "+result;
+    }
+
+    @RequestMapping("/getName")
+    public String getName() {
+        return name;
+    }
+
+    @RequestMapping("/getHttpUrl")
+    public String getHttpUrl(){
+        return httpUrl;
     }
 
 }
