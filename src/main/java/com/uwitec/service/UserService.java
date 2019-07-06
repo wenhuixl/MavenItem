6e01376f157011a7fbc9076efa2c26adf0f3d16b
wenhui <wenhuigl@gmail.com>
2019-07-06 20:42:41 +0800

    事务管理注解@Transactional

--- src/main/java/com/uwitec/service/UserService.java
+++ src/main/java/com/uwitec/service/UserService.java
@@ -4,6 +4,7 @@ import com.uwitec.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author wenhui
@@ -18,8 +19,10 @@ public class UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Transactional // 事务
     public int insert(String name, Integer age) {
         int result = userMapper.insert(name, age);
+        int i = 1 / age; // age=0时抛出异常，验证事务是否开启
         log.info("=====result: ====="+result);
         return result;
     }
